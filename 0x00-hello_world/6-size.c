f6227f875ffde90c404940b7966ed2173a4b329f
okoth124 <124okoth@gmail.com>
2023-07-13 19:59:15 -0700

    any

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
-
-
 /**
- * main - Prints the size of various types based on 
- * the computer it is compilied and run on
- * Return: Always 0.
+ * main - A program that prints the size of various comput
+er types
+ * Return 0 (Sucess)
  */
+int main(void)
+{	
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
 
-int main (void)
-
-{
-	printf("Size of a char: %zu byte(s)\n, sizeof(char));
-
-	printf("Size of an int: %zu byte(s)\n, sizeof(int));
-	printf("Size of a long int: %zu byte(s)\n, sizeof(long int));
-	printf("Size of a long long int: %zu byte(s)\n, sizeof(long long int));
-	printf("Size of a char: %zu byte(s)\n, sizeof(char));
-	printf("Size of a float: %zu byte(s)\n, sizeof(float));
-	return (0);
-}
+printf("Size of a char: %zu 1 byte(s)\n", (unsigned long)s
+izeof(a));
+printf("Size of a int: %zu 1 byte(s)\n", (unsigned long)s
+izeof(b));
+printf("Size of a long int: %zu 1 byte(s)\n", (unsigned long)s
+izeof(c));
+printf("Size of a long long int: %zu 1 byte(s)\n", (unsigned long)s
+izeof(d));
+printf("Size of a float: %zu 1 byte(s)\n", (unsigned long)s
+izeof(f));
