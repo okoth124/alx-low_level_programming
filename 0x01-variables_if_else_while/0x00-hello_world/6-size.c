dfda272eac6ed2d3b9e2be8e8198a5b296deabb8
okoth124 <124okoth@gmail.com>
2023-07-15 22:48:30 -0700

    Positive anything is better than negative nothing

---
+++ 0x01-variables_if_else_while/0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the size of various comput
+er types
+ * Return 0 (Sucess)
+ */
+int main(void)
+{	
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+printf("Size of a char: %zu 1 byte(s)\n", (unsigned long)s
+izeof(a));
+printf("Size of a int: %zu 1 byte(s)\n", (unsigned long)s
+izeof(b));
+printf("Size of a long int: %zu 1 byte(s)\n", (unsigned long)s
+izeof(c));
+printf("Size of a long long int: %zu 1 byte(s)\n", (unsigned long)s
+izeof(d));
+printf("Size of a float: %zu 1 byte(s)\n", (unsigned long)s
+izeof(f));
