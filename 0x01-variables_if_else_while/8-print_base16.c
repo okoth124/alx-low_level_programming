0b80426aa34e55372d98eba1ac3d3e109aecff94
okoth124 <124okoth@gmail.com>
2023-07-17 10:37:01 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -2,14 +2,14 @@
 
 int main(void)
 {
-	int r = 0;
-	int s =10;
+	int r = 45;
+	int s =97;
 
-	while (r <= 9);
+	while (r <= 57);
 		putchar(r);
-	while (s <= 15);
+	while (s <= 102);
 		putchar(s);
-	putchar('\n')
+	putchar('\n');
 						
 return(0);
 }
