84e23697595c86e5155c10e26ce0d9a3967de72b
okoth124 <124okoth@gmail.com>
2023-07-17 00:59:38 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * Main - Entry point
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+	char _c = 'a';
+	char _d = 'A';
+
+	while (_c = < 'z'){
+		putchar(x);
+	       }
+	while (_d = < 'Z'){
+		putchar(y);
+	       }
+	putchar('\n');
+	return (0);
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * Main - Entry point
+ * Description: program prints all digit numbers of base 10 starting frm 0
+ * Return always 0
+ */
+
+int main(void)
+{	
+	for (int h = 0; h <= 9; h++);
+	return (0);
+
+
+}
