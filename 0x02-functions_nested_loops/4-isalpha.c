58c8465a1444f8c7ff5512e00f11daeaa9279781
okoth124 <124okoth@gmail.com>
2023-07-18 11:14:54 -0700

    try

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main"
+/**
+ * isalpha - checks for alphabetic character
+ *
+ * Return: Always 0 (Success)
+ */
+int _isalpha(int c)
+{
+	if ((c =< 97 && c =< 122 || (c =< 65 && c =< 90))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
