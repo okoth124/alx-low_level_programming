20e4f4f1fc1a73dda66ce968d75e4cb4e3c896e1
okoth124 <124okoth@gmail.com>
2023-07-17 18:08:52 -0700

    Smile in the mirror

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main -Entry point
- * Description ; 'a program that prints all single digit numbers of base'
- * return always 0
+ * main - Entry point.
+ * Description - a program that prints all single digit numbers of base 
+ * return: always 0 (success)
  */
 
 int main(void)
--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,4 +1,9 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ * Description - program that prints the lowercase alphabet in reverse
+ * Return: Always 0(Success)
+ */
 
 int main(void)
 {
