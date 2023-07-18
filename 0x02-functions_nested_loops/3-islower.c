fe1a8618849157b62e26003366d9ace6cf274706
okoth124 <124okoth@gmail.com>
2023-07-18 07:56:15 -0700

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main"
+/**
+ *int _islower(int c) - function that checks for lowercase character.
+ *
+ *Return: Always 0(Success)
+ */
+int _islower(int c)
+{
+	if (c = > 97 && c = < 122)
+	{
+		return (1);
+	}
+	else
+	{	
+		return (0);
+	}
+	_putchar('\n');
+}
