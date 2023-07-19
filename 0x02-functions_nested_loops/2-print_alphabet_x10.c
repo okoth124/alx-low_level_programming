bae4d09a4cdc5afb393ea126415b6392c65118d2
okoth124 <124okoth@gmail.com>
2023-07-19 00:15:59 -0700

    try

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,16 +5,16 @@
  * Return: Always 0 (Success)
  */
 void print_alphabet_x10(void)
-
+{
 	int j;
 	int i;
 
 	for (j = 0; j <= 9; j++)
-	{	
+	{
 		for (i = 97; i <= 122; i++)
 		{
 			_putchar(i);
-		_putchar('\n');
 		}
+		_putchar('\n');
 	}
-
+}
