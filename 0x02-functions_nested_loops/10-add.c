1f25b32ecb3504ed9b6c8fce9944da134ffa1b0c
okoth124 <124okoth@gmail.com>
2023-07-19 07:45:19 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * add - adds 2 integers and return results
+ *@i: one of the digits being addeD
+ *@j: one of the  digits beinng added
+ * Return: Always 0 (Success)
+ */
+int add(int i, int j)
+{
+	int sum;
+
+	sum = i + j;
+	_putchar(sum)
+	return (0);
+}
