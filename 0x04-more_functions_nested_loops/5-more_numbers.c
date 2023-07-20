745bab99ff65824d15d86faee1a4b83ca35cff5e
okoth124 <124okoth@gmail.com>
2023-07-20 13:22:16 -0700

    Numbers constitute the only universal language

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,9 +10,10 @@ void more_numbers(void)
 
 	for (i = '0'; i <= '9'; i++)
 	{
-		for (j = '0'; j <= '14', j++)
+		for (j = '0'; j <= '14'; j++)
+		{
 			_putchar(j);
-	}
-
-
+		}
+		_putchar('\n');
+	}	
 }
