38c0c1a3238fd9889f0d72c8bc5cb202c2703aa4
okoth124 <124okoth@gmail.com>
2023-07-20 11:13:58 -0700

    Fizz-Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ *main - Entry point.
+ *Description -FizzBuZZ test
+ *Return: Always 0 (Succcess).
+ */
+int main(void)
+{
+	int 1;
+
+	for (i = 0; i <= 100; i++)
+	{
+		if ((i / 3) == 1)&&((i % 3) == 0)
+		{
+			_putchar('F');
+			_putchar('i');
+			_putchar('z');
+			_putchar('z');
+			_putchar('\n')
+			continue;
+		}
+		if ((i / 5) == 1)&&((i % 5) == 0)
+		{
+			_putchar('F');
+			_putchar('i');
+			_putchar('z');
+			_putchar('z');
+			_putchar('B');
+			_putchar('u');
+			_putchar('z');
+			_putchar('z');
+			_putchar('\n');
+			continue;
+		}
+	}
+		_putchar(i);
+		_putchar('\n');
+	return (0);
+}
