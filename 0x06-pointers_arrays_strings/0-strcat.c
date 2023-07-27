126334e0857084c3269fafd046f502544feea41c
okoth124 <124okoth@gmail.com>
2023-07-27 15:08:55 -0700

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * *_strcat - function concatnates 2 strings
+ * @src - input
+ * @dest - input
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src);
+{
+	int length_of_string, z;
+
+	length_of_string = 0;
+
+	while (dest[length_of_string] != '\0')
+	{
+		length_of_string++;
+					
+	}
+	for (z = 0; src[z] != '\0'; z++; length_of_string++)
+	{	
+		dest[length_of_string] = src[j];
+	}
+	dest[length_of_string] = '\0';
+	return (dest)
+}
